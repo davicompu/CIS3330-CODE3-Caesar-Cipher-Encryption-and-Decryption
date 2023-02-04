aba5ebd676283bb5c0041605182f03dfd19a53f0
Eduardo (David) Villacis Calderon <villacis@vt.edu>
2023-02-04 17:43:03 -0500

    Initial setup

---
+++ code_4.py
@@ -0,0 +1,9 @@
+
+def encrypt_message(message, key):
+   pass #Remove this line and code your solution HERE
+
+def decrypt_message(message, key):
+   pass #Remove this line and code your solution HERE
+   
+if __name__ == "__main__": #Do not remove this line
+    pass #Remove this line and code your solution HERE
